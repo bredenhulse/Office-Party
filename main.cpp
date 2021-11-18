70bdb2c5848a85fb53f7e5e791f61e1e20f176fb
bredenhulse <bredenhulse@gmail.com>
2021-11-18 08:21:37 -0600

    Just basics

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ifstream fin;
+    fin.open("input.txt");
+    ofstream fout;
+    fout.open("outfile");
+
+    int a, b;
+
+    a = 2;
+}
